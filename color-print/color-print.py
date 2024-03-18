ef60f76f8fa76c12ef042d57c927e621a2b0fb28
Nebeyou Musie <nebeyoumusie@gmail.com>
2024-03-18 08:49:16 +0300

    print coloured text

---
+++ color-print/color-print.py
@@ -0,0 +1,8 @@
+import colorama 
+from colorama import Fore, Back
+colorama.init(autoreset = True)
+
+
+print(Fore.BLUE + Back.YELLOW + "Hello my name is Nebeyou Musie" + Fore.YELLOW + Back.BLUE + "I am a developer")
+print(Back.CYAN + "Hello there")
+print(Fore.RED + Back.GREEN + "How are you")
